cc6e152e23b83acdef0278517ab84e7237af3843
Amanuel <amaanumararaa@gmail.com>
2025-03-09 14:27:23 +0300

    Implement delete functionality in MessagesScreen with ListItemDeleteAction

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FlatList, StyleSheet, View } from 'react-native';
 import ListItem from '../components/ListItem';
 import Screen from '../components/Screen';
 import ListItemSeparator from '../components/ListItemSeparator';
 import ListItemDeleteAction from '../components/ListItemDeleteAction';
 
-const messages = [
+const initialMessages = [
     {
         id: 1, 
         title: "T1",
@@ -21,6 +21,15 @@ const messages = [
 ]
 
 function MessagesScreen(props) {
+
+    const [messages, setMessages] = useState(initialMessages);
+
+    const handleDelete = message => {
+        setMessages(messages.filter(m => m.id !== message.id))
+    };
+
+    
+
     return (
         <Screen>
             <FlatList 
@@ -31,7 +40,7 @@ function MessagesScreen(props) {
                     subTitle={item.description}
                     image={item.image}
                     onPress={() => console.log('Message selected', item)}
-                    renderRightActions={ListItemDeleteAction}
+                    renderRightActions={() => <ListItemDeleteAction onPress={() => handleDelete(item)}/>}
                 />}
                 ItemSeparatorComponent={ListItemSeparator}
             />
