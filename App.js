8c77bf2bb5de1628624b9046d50695682bc0af26
Amanuel <amaanumararaa@gmail.com>
2025-03-09 12:37:49 +0300

    Add ListingDetailsScreen and ListItem components

--- App.js
+++ App.js
@@ -19,24 +19,13 @@ import AppButton from './app/components/AppButton';
 import WelcomeScreeen from './app/screens/WelcomeScreeen';
 
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/components/ListingDetailsScreen';
 
 export default function App() {
 
   return (
 
-    <View style ={{
-      backgroundColor: "#f8f4f4",
-      padding: 20,
-      paddingTop: 100,
-    }}>
-
-      <Card 
-      title = "Red jacket for sale"
-      subTitle = "$100"
-      image = {require("./app/assets/jacket.jpg")}
-      />
-
-    </View>
+    <ListingDetailsScreen />
 
   );
 }
