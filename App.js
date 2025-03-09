885bdceab511efef571d5ee7482794e7541bc3a9
Amanuel <amaanumararaa@gmail.com>
2025-03-09 13:07:43 +0300

    Add MessagesScreen component; include Screen component for layout

--- App.js
+++ App.js
@@ -21,12 +21,13 @@ import WelcomeScreeen from './app/screens/WelcomeScreeen';
 import Card from './app/components/Card';
 import ListingDetailsScreen from './app/components/ListingDetailsScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
+import MessagesScreen from './app/screens/MessagesScreen';
 
 export default function App() {
 
   return (
 
-    <ViewImageScreen />
+    <MessagesScreen />
 
   );
 }
