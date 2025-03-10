2667463a5655757074f5dba27f91060a435e25fe
Amanuel <amaanumararaa@gmail.com>
2025-03-10 17:07:05 +0300

    Add AppPicker component for category selection

--- App.js
+++ App.js
@@ -1,15 +1,32 @@
-import { Text, TextInput } from 'react-native';
+import { Switch, Text, TextInput } from 'react-native';
 import Screen from './app/components/Screen';
 import { useState } from 'react';
 import AppTextInput from './app/components/AppTextInput';
+import AppPicker from './app/components/AppPicker';
+
+const categories = [
+  {
+    label: "Furniture",
+    value: 1
+  },
+  {
+    label: "Clothing",
+    value: 2
+  },
+  {
+    label: "Cameras",
+    value: 3
+  },
+]
 
 export default function App() {
 
-  const [firstName, setFirstName] = useState('');
+  const [isNew, setIsNew] = useState(false);
 
   return (
     <Screen>
-      <AppTextInput placeholder = "Username" icon={'email'}/>
+      <AppPicker items = {categories} icon = "apps" placeholder={'Category'}/>
+      <AppTextInput icon= "email" placeholder='Email'/>
     </Screen>
   );
 }
---
+++ app/components/AppPicker.js
@@ -0,0 +1,65 @@
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { Button, FlatList, Modal, StyleSheet, TextInput, TouchableWithoutFeedback, View } from 'react-native';
+import defaultStyles from '../config/styles';
+import AppText from './AppText';
+import PickerItem from './PickerItem';
+
+function AppPicker({icon, placeholder, items}) {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    return (
+        <>
+            <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+                <View style = {styles.container}> 
+                    {icon && 
+                    <MaterialCommunityIcons 
+                    size={20} 
+                    color={defaultStyles.colors.medium} 
+                    style = {styles.icon} 
+                    name={icon}/>
+                    }
+                    <AppText style={styles.text}>{placeholder}</AppText>
+                    <MaterialCommunityIcons 
+                        size={20} 
+                        color={defaultStyles.colors.medium}
+                        name= "chevron-down"
+                    />
+                </View>
+            </TouchableWithoutFeedback>
+            <Modal visible = {modalVisible} animationType='slide'>
+                <Button title='Close' onPress={() => setModalVisible(false)}/>
+                    <FlatList 
+                        data={items}
+                        keyExtractor={item => item.value.toString()}
+                        renderItem={({item}) => <PickerItem
+                            label={item.label}
+                            onPress={() => console.log(item)}
+                        />}
+                    />
+            </Modal>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: defaultStyles.colors.light,
+        borderRadius: 25,
+        flexDirection: 'row',
+        width: '100%',
+        padding: 10,
+        marginVertical: 10,
+        alignItems: 'center' 
+    },
+
+    icon: {
+        marginRight: 10
+    },
+
+    text: {
+        flex: 1
+    }
+})
+
+export default AppPicker;
