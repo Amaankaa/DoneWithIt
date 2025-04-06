6fb914072e252847b8ddcc86f8ca3a23006c7b50
Amanuel <amaanumararaa@gmail.com>
2025-04-06 21:21:52 +0300

    Created a custom hook to get users location and put it in its own folder 'hook'

--- app/components/CategoryPickerItem.js
+++ app/components/CategoryPickerItem.js
@@ -1,30 +1,35 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Icon from './Icon';
-import AppText from './AppText';
+import React from "react";
+import { View, StyleSheet, TouchableOpacity } from "react-native";
+
+import Icon from "./Icon";
+import Text from "./AppText";
 
 function CategoryPickerItem({ item, onPress }) {
-    return (
-        <View style = {styles.container }>
-            <Icon backgroundColor={item.backgroundColor} name={item.icon} size={80}/>
-            <AppText style={styles.label}>{item.label}</AppText>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={onPress}>
+        <Icon
+          backgroundColor={item.backgroundColor}
+          name={item.icon}
+          size={80}
+        />
+      </TouchableOpacity>
+      <Text style={styles.label}>{item.label}</Text>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingHorizontal: 30,
-        paddingVertical: 15,
-        alignItems: 'center',
-        width: "33%"
-    },
-
-    label: {
-        marginTop: 5,
-        textAlign: 'center'
-    }
-})
+  container: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    alignItems: "center",
+    width: "33%",
+  },
+  label: {
+    marginTop: 5,
+    textAlign: "center",
+  },
+});
 
 export default CategoryPickerItem;
